b694ba6d2ab8216c99ea9740aa76c7252d30ab9c
Ksoy <ksoyam95@gmail.com>
2022-05-12 15:52:31 +0800

    new tests: buy 1 book

--- apps/potter/src/app/order.spec.ts
+++ apps/potter/src/app/order.spec.ts
@@ -1,4 +1,4 @@
-import { Order } from './order';
+import { basic_price, Order } from './order';
 
 describe('Order', () => {
   it('should create an instance', () => {
@@ -8,5 +8,35 @@ describe('Order', () => {
   it('order basic', () => {
     const order = new Order();
     expect(order.price).toBe(0);
+  })
+
+  it('order basic - b0 * 1', () => {
+    const order = new Order();
+    order.buy(0);
+    expect(order.price).toBe(basic_price);
+  })
+
+  it('order basic - b1 * 1', () => {
+    const order = new Order();
+    order.buy(1);
+    expect(order.price).toBe(basic_price);
+  })
+
+  it('order basic - b2 * 1', () => {
+    const order = new Order();
+    order.buy(2);
+    expect(order.price).toBe(basic_price);
+  })
+
+  it('order basic - b3 * 1', () => {
+    const order = new Order();
+    order.buy(3);
+    expect(order.price).toBe(basic_price);
+  })
+
+  it('order basic - b4 * 1', () => {
+    const order = new Order();
+    order.buy(4);
+    expect(order.price).toBe(basic_price);
   })
 });
