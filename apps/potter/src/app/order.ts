7d32d97b3e4f7d15cdad67629409257755e70583
Ksoy <ksoyam95@gmail.com>
2022-05-12 17:04:36 +0800

    refactor & fix: several discounts

--- apps/potter/src/app/order.ts
+++ apps/potter/src/app/order.ts
@@ -1,19 +1,32 @@
 export const basic_price = 8;
-export const discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+export const discount = [0, 1, 0.95, 0.9, 0.8, 0.75];
 
 export class Order {
-	private _books: number[] = [];
+	private _books = [0, 0, 0, 0, 0];
 	buy(book: number) {
-		this._books.push(book);
+		this._books[book] += 1;
 	}
 
 	get price() {
-		let len = this._books.length;
-		let uniqe_books = this._books.filter(unique);
-		return len * basic_price * discount[uniqe_books.length];
+		let cz, total = 0;
+
+		// use greedy discount to get price
+		while((cz = countZero(this._books)) != 5) {
+			total += basic_price * (5 - cz) * discount[5 - cz];
+			reduce(this._books);
+		}
+		return total;
 	}
 }
 
-function unique(value: any, index: number, self: Array<any>) {
-	return self.indexOf(value) === index;
+function countZero(books: Array<number>) {
+	return books.filter((book) => {
+		return book === 0;
+	}).length;
+}
+
+function reduce(books: Array<number>) {
+	for(let i = 0; i < books.length; i++) {
+		books[i] = Math.max(0, books[i] - 1)
+	}
 }
